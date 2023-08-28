5883940f6cbb04d8351952c98e67278b1b8b52e9
Baribor Saturday <saturdaybaribor@gmail.com>
2023-08-28 15:30:39 +0100

    Fixed a bug in task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    if x <= 0:
+        return x
     try:
         for i in range(x):
             print(my_list[i], end="")
@@ -13,7 +15,7 @@ def safe_print_list(my_list=[], x=0):
 if __name__ == '__main__':
     my_list = [1, 2, 3, 4, 5]
 
-    nb_print = safe_print_list(my_list, 2)
+    nb_print = safe_print_list(my_list, 0)
     print("nb_print: {:d}".format(nb_print))
     nb_print = safe_print_list(my_list, len(my_list))
     print("nb_print: {:d}".format(nb_print))
