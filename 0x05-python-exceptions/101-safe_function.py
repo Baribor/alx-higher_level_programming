2db1c0a9687d9af3a53cf95e82927ce861f565eb
Baribor Saturday <saturdaybaribor@gmail.com>
2023-08-28 15:25:26 +0100

    Fixed all pycodestyle error

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_function(fct, *args):
     try:
         return fct(*args)
@@ -19,7 +20,6 @@ if __name__ == '__main__':
     result = safe_function(my_div, 10, 0)
     print("result of my_div: {}".format(result))
 
-
     def print_list(my_list, len):
         i = 0
         while i < len:
@@ -29,4 +29,3 @@ if __name__ == '__main__':
 
     result = safe_function(print_list, [1, 2, 3, 4], 10)
     print("result of print_list: {}".format(result))
- 
