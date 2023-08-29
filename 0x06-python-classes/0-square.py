f27d237ff5449b6dd6408357905cc41d7a09ca44
Baribor Saturday <saturdaybaribor@gmail.com>
2023-08-29 14:31:57 +0100

    Solved task 0: My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
