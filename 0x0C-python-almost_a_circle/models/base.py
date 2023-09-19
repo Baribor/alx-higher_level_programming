151d832fadf5048decc87f9cbaa06e2035cee3cc
Baribor Saturday <saturdaybaribor@gmail.com>
2023-09-19 01:21:14 +0100

    Added docstring

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
+"""
+The `base` module.
+The module defines the base class for all shapes/
+"""
 
 
 class Base:
+    """The Base class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """class constructor
+
+        Args:
+            id (int, optional): The id of the shape. Defaults to None.
+        """
 
         if not id:
             Base.__nb_objects += 1
@@ -14,6 +24,14 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """returns the json string of shapes
+
+        Args:
+            list_dictionaries (list): list of dict
+
+        Returns:
+            str: json string
+        """
         if len(list_dictionaries) == 0:
             return '[]'
         json = ['[']
@@ -35,6 +53,11 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """saves shapes to file in json format
+
+        Args:
+            list_objs (list): list of obj
+        """
         list_of_dict = []
         for obj in list_objs:
             list_of_dict.append(obj.to_dictionary())
